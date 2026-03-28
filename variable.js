335444efd867fe75ee6779b0dadea28be99c6c17
Vishwajeet Singh <vishwajeetsingh9449@gmail.com>
2026-03-28 11:23:13 +0530

    Type of variable in JS

--- variable.js
+++ variable.js
@@ -1,1 +1,10 @@
+// To Declare Variable in Javascript we use three type of variable
+// 1. var
+// 2. let
+// 3. const
 
+const user_id = 001;
+let user_name = "Vishwajeet Singh;
+var user_country = "INDIA";
+
+// THANKS FOR VISITING 
