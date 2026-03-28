dde7e4f9b00acdc032d7d07064d1b0c4a55711df
Vishwajeet Singh <vishwajeetsingh9449@gmail.com>
2026-03-28 11:46:54 +0530

    Document data types in JavaScript
    Added comments explaining primitive and non-primitive data types in JavaScript.

--- data-type.js
+++ data-type.js
@@ -1,1 +1,42 @@
+// Type of data in JavaScript 
 
+// a. PRIMITIVE DATA TYPE 
+
+// 1. String 
+let user_name = "Vishwajeet Singh";
+
+// 2. Number
+let user_age = 23;
+
+// 3. Boolean
+let is_user_male = true;
+
+// 4. BigInt 
+let user_id = BigInt("999999999999999999999999999999999999999");
+
+// 5. Undefined
+let user_xyz = undefined;
+
+// 6. Null
+let user_networth = null;
+
+// 7. Symbol
+let sym = Symbol("id");
+
+
+// b. NON-PRIMITIVE DATA TYPE 
+
+// 8. Array
+let user_qualification = ["MCA","BCA"];
+
+// 9. Object
+let userJobDetail = {
+  job_title: "Web Developer",
+  skill: ["HTML","CSS","JavaScript","WordPress","MySQL","PHP","Bootstrap","Many More"],
+  job_location: "NOIDA"
+}
+
+// 10. Function
+function greet(){
+    return "Hello";
+}
